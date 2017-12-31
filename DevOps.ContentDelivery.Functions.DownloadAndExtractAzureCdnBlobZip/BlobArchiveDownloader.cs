054d8e6ed768a2fa571511c097a1dbedd5b1c091
Christopher Dorst <chris@dorst.email>
2017-12-30 22:52:19 -0600

    Add BlobArchiveDownloader

---
+++ DevOps.ContentDelivery.Functions.DownloadAndExtractAzureCdnBlobZip/BlobArchiveDownloader.cs
@@ -0,0 +1,14 @@
+﻿using DevOps.ContentDelivery.Structures.AzureBlob;
+using static Common.Functions.DownloadAndExtractZip.ZipDownloaderAndExtractor;
+using static DevOps.ContentDelivery.Functions.ResolveAzureCdnBlobUrl.UrlResolver;
+
+namespace DevOps.ContentDelivery.Functions.DownloadAndExtractAzureCdnBlobZip
+{
+    public static class BlobArchiveDownloader
+    {
+        public static void Extract(string endpointName, Blob blob, string destinationPath)
+            => DownloadAndExtract(
+                Uri(endpointName, blob),
+                destinationPath);
+    }
+}
